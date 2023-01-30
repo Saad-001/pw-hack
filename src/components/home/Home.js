02d7163f2bff8476c96fb395085a08a42ee81813
Saad-001 <saadafnan21@gmail.com>
2023-01-30 15:20:11 +0600

    home page UI updated

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,22 @@
 import React from "react";
+import Image from "react-bootstrap/Image";
+import heroImage from "../../assets/luke-chesser-JKUTrJ4vK00-unsplash.jpg";
 import Layout from "../layout/Layout";
 
 const Home = () => {
   return (
     <Layout>
-      <div>Home</div>
+      <div className="d-flex justify-content-between align-items-center mt-5">
+        <div className="">
+          <h1>Experience The Best Survice</h1>
+          <h4>
+            No Metter Wherever You're, We Are <br /> Right Near You
+          </h4>
+        </div>
+        <div style={{ maxWidth: "600px", maxHeight: "600px" }} className="">
+          <Image fluid src={heroImage} rounded alt="" />
+        </div>
+      </div>
     </Layout>
   );
 };
